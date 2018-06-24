cddfe2a77e76bc86ceb865866807e9f450ca81f2
steven9587 <steven9587@gmail.com>
2018-06-24 19:25:41 +0800

    snow-final

--- src/snow/Snow.java
+++ src/snow/Snow.java
@@ -1,5 +1,36 @@
 package snow;
 
-public class Snow {
+import java.util.Random;
 
+import snow.Snowframe.SnowCanvas;
+
+public class Snow extends Thread {
+	int x;
+	int y;
+	Random r = new Random();
+	private SnowCanvas snowCanvas;
+
+	public Snow(SnowCanvas snowCanvas) {
+		this.snowCanvas=snowCanvas;
+		x = r.nextInt(800);
+		y = r.nextInt(600);
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			y += 1;
+			x += r.nextInt(3) - 1;
+			snowCanvas.repaint();
+			try {
+				sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (y == 600) {
+				y = 0;
+			}
+		}
+	}
 }
--- src/snow/Snowframe.java
+++ src/snow/Snowframe.java
@@ -1,9 +1,49 @@
 package snow;
 
-public class Snowframe {
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Snowframe extends JFrame {
+	SnowCanvas canvas = new SnowCanvas();
+
+	public Snowframe() {
+		setSize(850, 650);
+		setLocation(300, 300);
+		add(canvas);
+		setVisible(true);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+
+	public class SnowCanvas extends JPanel{
+		List<Snow> snows = new ArrayList<>();
+
+		public SnowCanvas() {
+			setBackground(Color.BLACK);
+			for (int i = 0; i < 100; i++) {
+				Snow snow = new Snow(this);
+				snows.add(snow);
+				snow.start();
+			}
+		}
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			for(int i=0;i<100;i++) {
+				Snow snow = snows.get(i);
+				g.setColor(Color.WHITE);
+				g.fillOval(snow.x, snow.y, 10, 10);
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-
+		new Snowframe();
 	}
 
 }
